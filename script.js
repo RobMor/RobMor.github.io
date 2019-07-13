49b2e535c7d86fb43d3953bbbe8d298347fab5eb
Robbie Morrison <robbieguy98@gmail.com>
2019-07-13 02:24:17 -0400

    Updated index and included smooth-scrolling scripts

---
+++ script.js
@@ -0,0 +1,12 @@
+function applySmoothScroll() {
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
+
+window.onload = applySmoothScroll
